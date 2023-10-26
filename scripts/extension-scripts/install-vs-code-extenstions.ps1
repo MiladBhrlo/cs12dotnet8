89df4125b7a5ba6906cbd49daef8eb9b0549b95a
MiladBhrlo <BornWish@outlook.com>
2023-10-26 10:36:57 +0330

    first commit

---
+++ scripts/extension-scripts/install-vs-code-extenstions.ps1
@@ -0,0 +1,10 @@
+# C# Dev Kit
+code --install-extension ms-dotnettools.csdevkit
+# Polyglot Notebooks
+code --install-extension ms-dotnettools.dotnet-interactive-vscode
+# MSBuild project tools
+code --install-extension tintoy.msbuild-project-tools
+# REST Client
+code --install-extension humao.rest-client
+# ilspy-vscode
+code --install-extension icsharpcode.ilspy-vscode
